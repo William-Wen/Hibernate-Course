28ac0da6037b55afc0b73fc3e7345ed04dc91ad9
William <william.wen@tac.com.tw>
2016-11-07 09:37:41 +0800

    Section 05, Chapter 29 Mapping Collections of Basic Value Types is finished.

--- src/main/java/com/infiniteskills/data/Application.java
+++ src/main/java/com/infiniteskills/data/Application.java
@@ -3,8 +3,12 @@ package com.infiniteskills.data;
 import java.util.Calendar;
 import java.util.Date;
 
-import org.hibernate.Session;
 
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.infiniteskills.data.entities.Address;
+import com.infiniteskills.data.entities.Bank;
 import com.infiniteskills.data.entities.TimeTest;
 import com.infiniteskills.data.entities.User;
 
@@ -36,28 +40,71 @@ public class Application {
 		
 
 		try {
-			session.getTransaction().begin();
+//			session.getTransaction().begin();
 			
 //			session.beginTransaction();
 //			TimeTest oTimeTest = (TimeTest) session.get(TimeTest.class, new Long(8));
 //			System.out.println("TimeTest: " + oTimeTest.toString());
 			
-			User oUser = new User();
-			oUser.setBirthDate(getMyBirthday());
-			oUser.setCreatedBy("William");
-			oUser.setCreatedDate(new Date());
-			oUser.setEmailAddress("william.wen@tac.com.tw");
-			oUser.setFirstName("Wen");
-			oUser.setLastName("William");
-			oUser.setLastUpdatedBy("Henry");
-			oUser.setLastUpdatedDate(new Date());
+//			User oUser = new User();
+//			oUser.setBirthDate(getMyBirthday());
+//			oUser.setCreatedBy("William");
+//			oUser.setCreatedDate(new Date());
+//			oUser.setEmailAddress("william.wen@tac.com.tw");
+//			oUser.setFirstName("Wen");
+//			oUser.setLastName("William");
+//			oUser.setLastUpdatedBy("Henry");
+//			oUser.setLastUpdatedDate(new Date());
+//			
+//			session.save(oUser);
+//			session.getTransaction().commit();
+//			
+//			session.refresh(oUser);
+//			
+//			System.out.println("William's age is " + oUser.getAge());
 			
-			session.save(oUser);
-			session.getTransaction().commit();
+			Transaction oTransaction = session.beginTransaction();
 			
-			session.refresh(oUser);
+			Bank oBank = new Bank();
+			oBank.setName("Federal Trust");
+			oBank.setAddressLine1("33 Wall Street");
+			oBank.setAddressLine2("Suite 302");
+			oBank.setCity("New York");
+			oBank.setState("NY");
+			oBank.setZipCode("27914");
+			oBank.setCreatedBy("Kevin BowerSox");
+			oBank.setCreatedDate(new Date());
+			oBank.setLastUpdatedBy("Kevin Bowersox");
+			oBank.setLastUpdatedDate(new Date());
+			oBank.setInternational(false);
 			
-			System.out.println("William's age is " + oUser.getAge());
+			oBank.getContacts().add("Contact 1");
+			oBank.getContacts().add("Contact 2");
+			
+			session.save(oBank);
+			
+//			User oUser = new User();
+//			Address oAddress = new Address();
+//			oUser.setAge(46);
+//			oUser.setBirthDate(new Date());
+//			oUser.setCreatedBy("William");
+//			oUser.setCreatedDate(new Date());
+//			oUser.setEmailAddress("william.wen@");
+//			oUser.setFirstName("William");
+//			oUser.setLastName("Wen");
+//			oUser.setLastUpdatedBy("William");
+//			oUser.setLastUpdatedDate(new Date());
+//			
+//			oAddress.setAddressLine1("Line1");
+//			oAddress.setAddressLine2("Line2");
+//			oAddress.setCity("Philadelphia");
+//			oAddress.setState("PA");
+//			oAddress.setZipCode("12345");
+//			
+//			oUser.setAddress(oAddress);
+//			session.save(oUser);
+			
+			oTransaction.commit();
 			
 		} catch (Exception e) {
 			// TODO: handle exception
